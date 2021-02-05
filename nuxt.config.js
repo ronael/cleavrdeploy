15cb6d4d1aa83b8295416ba06e2ae39b11e92501
ronael <ronaeldat1@gmail.com>
2021-02-06 00:53:32 +0100

    server port

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  server: {     
+    port: 7800, // default: 3000     
+    // host: '0.0.0.0', // default: localhost   
+  },   // other configs
+
   head: {
     title: 'cleavrdeploy',
     htmlAttrs: {
